0f77741e1f1cca6948608157c30e731b96b59190
PHAM VAN DUNG <dungphamvanhp@gmail.com>
2016-12-29 16:16:06 +0700

    ok

--- TMWebsite/Migrations/Configuration.cs
+++ TMWebsite/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
-namespace TMWebsite.Migrations
+﻿namespace TMWebsite.Migrations
 {
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -21,13 +22,12 @@ namespace TMWebsite.Migrations
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
             //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            //context.People.AddOrUpdate(
+            //  p => p.FullName,
+            //  new Person { FullName = "Andrew Peters" },
+            //  new Person { FullName = "Brice Lambson" },
+            //  new Person { FullName = "Rowan Miller" }
+            //);            
         }
     }
 }
